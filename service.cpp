140bf6183fde80b971f12d856e9682d169089c81
Anders Schau Knatten <anders@knatten.org>
2018-05-19 20:30:44 +0200

    initial commit

---
+++ service.cpp
@@ -0,0 +1,33 @@
+#include <service>
+#include <mpi.h>
+#include <library.h>
+#include <lammps.h>
+
+using namespace LAMMPS_NS;
+
+/* extern "C" { */
+/* void _lammps_open_no_mpi(int, char**, void**); */
+/* void _lammps_commands_string(void*, char*); */
+/* } */
+
+void Service::start(const std::string& )
+{
+    LAMMPS *lol = new LAMMPS(0, nullptr,MPI_COMM_WORLD);
+    void *lammps;
+    lammps_open_no_mpi(0, nullptr, &lammps);
+    char *script = "units    lj\n \
+    atom_style    atomic\n \
+    lattice        fcc 0.8442\n \
+    region        box block 0 20 0 20 0 20\n \
+    create_box    1 box\n \
+    create_atoms    1 box\n \
+    mass        1 1.0\n \
+    velocity    all create 1.44 87287\n \
+    pair_style    lj/cut 2.5\n \
+    pair_coeff    1 1 1.0 1.0 2.5\n \
+    neigh_modify every 1 delay 0 check yes\n \
+    thermo 10\n \
+    fix        1 all nve\n \
+    run        100";
+    //lammps_commands_string(lammps, script);
+}
